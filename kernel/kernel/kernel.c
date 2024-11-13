660ad2bae7e7cd07e4f475394487d7b728e1be8c
Lion <dannhorn@duck.com>
2024-11-13 22:02:05 +0100

    :3

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+//#include "vga.h"
 
 void kernel_main(void) {
 	terminal_initialize();
+
 	while (1==1) {
+		terminal_setcolor( 11 | 8 << 4 );
 		printf("Hallo Welt\n");
+
+		terminal_setcolor( 10 | 1 << 4 );
 		printf("Ich bin ein Betriebssystem!\n");
+
+		terminal_setcolor( 13 | 0 << 4 );
 		printf("Ein dritter Text.\n");
 	}
+
 }

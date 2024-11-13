406d3e0a535070f12317fd35b05e054cf4ca2a9b
Lion <dannhorn@duck.com>
2024-11-13 21:18:29 +0100

    myos2

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/myos2.kernel isodir/boot/myos2.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "myos2" {
+	multiboot /boot/myos2.kernel
 }
 EOF
-grub2-mkrescue -o myos.iso isodir
+grub2-mkrescue -o myos2.iso isodir

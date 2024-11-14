2a4a5b4404883eb313ecd6004acee7f744b48f12
Lion <dannhorn@duck.com>
2024-11-14 17:31:14 +0100

    vga.h Import in kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
-//#include "vga.h"
+#include "../arch/i386/vga.h"
 
 void kernel_main(void) {
 	terminal_initialize();
 
 	while (1==1) {
-		terminal_setcolor( 11 | 8 << 4 );
+		terminal_setcolor( vga_entry_color( VGA_COLOR_LIGHT_CYAN, VGA_COLOR_DARK_GREY ) );
 		printf("Hallo Welt\n");
 
-		terminal_setcolor( 10 | 1 << 4 );
+		terminal_setcolor( vga_entry_color( VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLUE ) );
 		printf("Ich bin ein Betriebssystem!\n");
 
-		terminal_setcolor( 13 | 0 << 4 );
+		terminal_setcolor( vga_entry_color( VGA_COLOR_LIGHT_MAGENTA, VGA_COLOR_BLACK ) );
 		printf("Ein dritter Text.\n");
 	}
 
